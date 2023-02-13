e3c27d1a75589d86ee195f0cf05f66b90a9b491a
eB00k <dasakatov8@gmail.com>
2023-02-14 00:32:47 +0600

    OnClick

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 const Home = () => {
-    return (  
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-    );
-}
- 
+  const handleClick = (e) => {
+    console.log("hello, Dastan");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log(`Hello, ${name}`);
+  };
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("Alish")}>Click me again</button>
+    </div>
+  );
+};
+
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,7 @@ const Navbar = () => {
       <h1>The Dass Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-            color: "white",
-            backgroundColor: "#f1356d",
-            borderRadius: "6px",
-          }}
-        >
-          New Blog
-        </a>
+        <a href="/create">New Blog</a>
       </div>
     </nav>
   );
