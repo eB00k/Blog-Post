8a0eb6edc1f38ebe67522c935cdafa43e8c71d41
eB00k <dasakatov8@gmail.com>
2023-02-14 09:11:20 +0600

    Added blog used .map & key to list elements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 
 const Home = () => {
-  // useState() helps us to make reactive value
-  const [name, setName] = useState("Dastan");
-  const [age, setAge] = useState(19);
-
-  const changeName = (value) => {
-    setName("Alish");
-    setAge(20);
-  };
-
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem impsum ...",
+      author: "Dastan",
+      id: 1,
+    },
+    {
+      title: "Welcome Party",
+      body: "lorem impsum ...",
+      author: "Aliher",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem impsum ...",
+      author: "Islam",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old!
-      </p>
-      <button onClick={changeName}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
