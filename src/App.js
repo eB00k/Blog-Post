dcd2596c6bc022b2496ceec9a7d5936b196247fa
eB00k <dasakatov8@gmail.com>
2023-02-14 21:44:27 +0600

    React Router router links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Create from "./components/Create";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
